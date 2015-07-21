340c734db92f85c7e4c5ec5ee2372ddc508f0156
SuhaylTFaris <chaosluigi92@hotmail.com>
2015-07-21 13:33:21 -0600

    Added getElementsByTagName code for step 2 of the exercise.

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ console.log("sanity check!");
 
 var firstParagraph = document.getElementById('main');
 console.log(firstParagraph);
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements)
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement)
